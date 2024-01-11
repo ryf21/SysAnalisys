0bed714f992448ecafd29095f1c1cf099f08377e
Роман <117450234+ryf21@users.noreply.github.com>
2024-01-11 12:55:41 +0300

    Update task.py

--- task1/task.py
+++ task1/task.py
@@ -1,23 +1,12 @@
 import csv
+import sys
 
-def read_csv_cell(file_path, row_index, column_index):
-    with open(file_path, newline='') as csvfile:
-        reader = csv.reader(csvfile)
-        data = list(reader)
+with open(sys.argv[1], 'r') as f:
+    lineIdx = int(sys.argv[2])
+    columnIdx = int(sys.argv[3])
+    reader = csv.reader(f)
 
-        if row_index <= len(data) and column_index <= len(data[row_index-1]):
-            cell_value = data[row_index-1][column_index-1]
-            return cell_value
-        else:
-            return None
-
-file_path = input()
-row_index = int(input())
-column_index = int(input())
-
-cell_value = read_csv_cell(file_path, row_index, column_index)
-if cell_value is not None:
-    print("The value in cell ({}, {}) is: {}".format(row_index, column_index, cell_value))
-else:
-    print("Invalid row or column index.")
+    for i, row in enumerate(reader):
+        if i == lineIdx:
+            print(row[columnIdx])
 
